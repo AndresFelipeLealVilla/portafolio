ef4badc19f74a449fb8e4f16310e457bb936f900
AndresFelipeLealVilla <andres.leal@correounivalle.edu.co>
2022-11-11 20:42:20 -0500

    first commit

--- src/componentes/pages/Portafolio.jsx
+++ src/componentes/pages/Portafolio.jsx
@@ -1,26 +1,12 @@
 import React from 'react';
 import Bloques from '../bloques/Bloques';
 import './pages.css';
+import '../bloques/Bloques.css';
 
 export const Portafolio = () => {
   return (
     <div className='portafolioDiv' >
-      <div style={{
-        position: 'relative',
-        width: '94%',
-        height: '100%',
-        margin: 'auto',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column',
-        backgroundColor: 'white',
-        borderRadius: '10px',
-        boxShadow: '0 0 10px 0 rgba(0,0,0,0.5)',
-        padding: '20px',
-        marginTop: '20px',
-        marginBottom: '20px',
-      }}>
+      <div className='uno'>
 
       </div>
 
