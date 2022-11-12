aca85e94aa48771099ec4e25f361bc445f86ff6e
AndresFelipeLealVilla <andres.leal@correounivalle.edu.co>
2022-11-12 01:37:07 -0500

    first commit

--- src/componentes/pages/Portafolio.jsx
+++ src/componentes/pages/Portafolio.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import Bloques from '../bloques/Bloques';
+import { NavLink as Link } from 'react-router-dom';
 import './pages.css';
 import '../bloques/Bloques.css';
 
 export const Portafolio = () => {
   return (
     <div className='portafolioDiv' >
+      <Link to='/uno'>
       <div className='uno'>
-
+        <a href='/uno'>
+        <span>
+          hola
+        </span>
+        </a>
+        <a href='/uno'>
+        <span>
+          adios
+        </span>
+        </a>
       </div>
+      </Link>
 
       <div>
 
